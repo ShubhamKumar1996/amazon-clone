f160823c9cf82c5a336ddc68445a487c94283e18
ShubhamKumar1996 <Nimcetshubhamkumar@gmail.com>
2022-03-16 14:16:07 +0530

    Added Header with search bar and other options

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBL69uGS-SH9J3bM5fnz4mg8V32T9VwDi0",
+    authDomain: "clone-7bebb.firebaseapp.com",
+    projectId: "clone-7bebb",
+    storageBucket: "clone-7bebb.appspot.com",
+    messagingSenderId: "490037656709",
+    appId: "1:490037656709:web:b70fd6453422ff0d777dec",
+    measurementId: "G-T07QTN25DG"
+  };
